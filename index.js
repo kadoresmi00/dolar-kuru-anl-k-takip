1966e5a8fb2f08885638c480e76d0562affc395b
Kado <kadoresmi00@gmail.com>
2021-12-21 00:10:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const fetch = require("node-fetch")
+
+const app = express()
+const http = require("http").createServer(app)
+const io = require("socket.io")(http)
+var Data
+
+let fetchCurrency = async() => {
+    try {
+        fetch('https://cancaliskan-doviz-api.herokuapp.com/', { method: "Get" }).then(res => res.json())
+            .then((json) => {
+                Data = json.Dollar
+            });
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+let currencyPoller = async() => {
+    await fetchCurrency()
+    setTimeout(async() => {
+        await fetchCurrency()
+        currencyPoller()
+    }, 1000);
+}
+
+currencyPoller()
+
+io.on("connection", (socket) => {
+    let socketPoller = () => {
+        socket.emit('currency-data', Data)
+        setTimeout(() => {
+            socket.emit('currency-data', Data)
+            socketPoller()
+        }, 1000);
+    }
+
+    socketPoller()
+
+    socket.on("disconnect", () => {})
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/kado.html');
+});
+
+http.listen(80, () => {
+    console.log(`Kado`)
+})
